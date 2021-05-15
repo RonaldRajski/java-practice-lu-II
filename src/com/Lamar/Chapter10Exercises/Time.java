40a2face2bf7be1dc08e93f4f496a64b9c7467bd
Ronald Rajski <ronaldrajski@gmail.com>
2021-05-15 08:09:35 -0500

    Time class

---
+++ src/com/Lamar/Chapter10Exercises/Time.java
@@ -0,0 +1,29 @@
+package com.Lamar.Chapter10Exercises;
+
+//        UML DIAGRAM
+
+//****************************************
+            TIME
+//
+//-hour: long
+//-minute: long
+//-second: long
+//Time()
+//Time(elapseTime: long)
+//Time(hour:  long minute: long second)
+//getHour(): long
+//getMinute():long
+//getSecond(): long
+//setTime(elapseTime: long)
+
+//****************************************
+
+
+public class Time {
+        private long hour;
+        private long minute;
+        private long second;
+
+
+
+}
