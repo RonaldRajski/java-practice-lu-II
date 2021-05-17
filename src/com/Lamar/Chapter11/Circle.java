82b775d6a385018b6c512c13be629f10b2ec4c6b
Ronald Rajski <ronaldrajski@gmail.com>
2021-05-17 08:22:03 -0500

    Circle finished

--- src/com/Lamar/Chapter11/Circle.java
+++ src/com/Lamar/Chapter11/Circle.java
@@ -1,4 +1,50 @@
 package com.Lamar.Chapter11;
 
-public class Circle {
+public class Circle extends GeometricObject{
+    private double radius;
+
+    public Circle(){
+
+    }
+
+    public Circle(double radius){
+        this.radius =  radius;
+    }
+
+    public Circle(double radius, String color, boolean filled){
+        this.radius = radius;
+        setColor(color);
+        setFilled(filled);
+    }
+
+    //    return radius
+    public double getRadius() {
+        return radius;
+    }
+
+    //    set new radius
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    //    return Area
+    public double getArea(){
+        return radius * radius * Math.PI;
+    }
+
+
+//    return diameter
+    public double getDiameter(){
+        return 2 * radius;
+    }
+
+
+//    return Perimeter
+    public double getPerimeter(){
+        return 2 * radius * Math.PI;
+    }
+
+    public void printCircle(){
+        System.out.println("The circle is created " + getDateCreated() + " and the radius is " + radius);
+    }
 }
